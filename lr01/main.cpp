d3d7a3fba3389936311a5e67a74affd6011bdb38
Krasin1 <dunaevv84@gmail.com>
2023-02-27 12:00:33 +0300

    все хуйня, давай по новой

--- lr01/main.cpp
+++ lr01/main.cpp
@@ -2,104 +2,12 @@
 #include <fstream>  //для записи в файл
 #include <iomanip>  //для setprecision
 #include <iostream>
-#include <thread>
-#include <vector>
-
-#define N 8500
-#define repeats 100000
-#define cnt_thread 12
 
 template <typename T>
-class sort {
-   public:
-    sort() : arr(new T*[cnt_thread]), time_array(new double[repeats]) {
-        for (size_t i{}; i < cnt_thread; ++i) {
-            arr[i] = new T[N];
-            generate_new_arr(arr[i]);
-        }
-    }
-
-    void generate_new_arr(T* arr) {
-        srand((unsigned)time(NULL));
-        for (size_t i = 0; i < N; ++i) {
-            arr[i] = rand() % N;
-        }
-    }
-
-    void time_to_file(const char* sort_name) {
-        std::string name_dot_format(sort_name);
-        std::ofstream file(name_dot_format.append(".txt"));
-        file << std::setprecision(6) << std::fixed;
-        file << "Размер массива : " << N << " | Кол-во прогонов: " << repeats
-             << "\n";
-        for (size_t i = 0; i < repeats; ++i) {
-            file << time_array[i] << '\n';
-        }
-    }
-
-    T* get_arr() { return arr; }
-    T* get_time_array() { return time_array; }
-
-    void measure_time(const char* (*func_sort)(T*, int, int)) {
-        std::cout << "Размер массива : " << N
-                  << " | Кол-во прогонов: " << repeats << "\n";
-        double sum = 0;
-        std::string func_name;
-        std::vector<std::thread> threads;
-        for (size_t i{}; i < cnt_thread; ++i) {
-            threads.emplace_back([i, func_sort, this, &func_name, &sum]() {
-                for (size_t j = (repeats / cnt_thread) * i;
-                     j < (repeats / cnt_thread) * (i + 1); ++j) {
-                    generate_new_arr(arr[i]);
-
-                    clock_t begin = clock();
-                    func_name = func_sort(arr[i], 0, N - 1);
-                    clock_t end = clock();
-
-                    time_array[j] = (double)(end - begin) / CLOCKS_PER_SEC;
-                    sum += time_array[j];
-                    // std::cout << j << " | " << time_array[j] << '\n';
-                    // std::cout << std::setprecision(3) << std::fixed
-                    //           << "Проценты : " << ((double)j / repeats) * 100
-                    //           << "% | Кол-во прогонов : " << j << " \r"
-                    //           << std::flush;
-                }
-            });
-        }
-        for (size_t i{}; i < cnt_thread; ++i) {
-            threads[i].join();
-        }
-        std::cout << std::setprecision(6)
-                  << "\rВремя сортировки за все прогоны : " << sum
-                  << " сек. | Среднее время " << func_name << " : "
-                  << sum / repeats << " сек.          \n";
-        time_to_file(func_name.data());
-    }
-
-    void show_arr() {
-        for (size_t i{}; i < N; ++i) {
-            std::cout << arr[i] << '\n';
-        }
-    }
-
-    ~sort() {
-        for (size_t i{}; i < cnt_thread; ++i) {
-            delete[] arr[i];
-        }
-        delete[] arr;
-        delete[] time_array;
-    }
-
-   private:
-    T** arr;
-    double* time_array;
-};
-
-template <typename T>
-const char* bubble_sort(T* a, int, int size = N - 1) {
+void bubble_sort(T* a, size_t size) {
     T temp;
-    for (int i{}; i < size - 1; ++i) {
-        for (int j{}; j < size - i - 1; ++j) {
+    for (size_t i{}; i < size - 1; ++i) {
+        for (size_t j{}; j < size - i - 1; ++j) {
             if (a[j] > a[j + 1]) {
                 temp = a[j];
                 a[j] = a[j + 1];
@@ -107,12 +15,11 @@ const char* bubble_sort(T* a, int, int size = N - 1) {
             }
         }
     }
-    return static_cast<const char*>(__FUNCTION__);
 }
 
 template <typename T>
-const char* hoarasort(T* a, int first_ind = 0, int last_ind = N - 1) {
-    int i = first_ind, j = last_ind;
+void hoarasort(T* a, size_t first_ind, size_t last_ind) {
+    size_t i = first_ind, j = last_ind;
     T tmp, x = a[(first_ind + last_ind) / 2];
 
     do {
@@ -132,14 +39,53 @@ const char* hoarasort(T* a, int first_ind = 0, int last_ind = N - 1) {
 
     if (i < last_ind) hoarasort(a, i, last_ind);
     if (first_ind < j) hoarasort(a, first_ind, j);
+}
 
-    return static_cast<const char*>(__FUNCTION__);
+template <typename T>
+void generate_new_arr(T* arr, size_t elem) {
+    srand((unsigned)time(NULL));
+    for (size_t i = 0; i < elem; ++i) {
+        arr[i] = rand() % (int)elem;
+    }
 }
 
 auto main() -> int {
-    sort<int> test;
-    test.measure_time(bubble_sort);
-    test.measure_time(hoarasort);
-    // test.show_arr();
+    const size_t N = 1000, repeats = 10000, step = 30;
+    int* arr_hoar = new int[N];
+    int* arr_bubble = new int[N];
+    double sum_hoar{}, sum_bubble{};
+
+    std::ofstream hoar("hoarsort.txt");
+    std::ofstream bubble("bubble.txt");
+    hoar << std::setprecision(6) << std::fixed
+         << "Время в микросекуднах | Размер массива \n";
+    bubble << std::setprecision(6) << std::fixed
+           << "Время в микросекуднах | Размер массива \n";
+
+    for (size_t i{step}, cnt{1}; i < N; i += step, ++cnt) {
+        generate_new_arr(arr_bubble, i);
+        generate_new_arr(arr_hoar, i);
+        sum_hoar = 0;
+        sum_bubble = 0;
+
+        for (size_t j{}; j < repeats; ++j) {
+            clock_t begin = clock();
+            hoarasort(arr_hoar, 0, i + 1);
+            clock_t end = clock();
+            sum_hoar += (double)(end - begin) / CLOCKS_PER_SEC;
+
+            begin = clock();
+            bubble_sort(arr_bubble, i);
+            end = clock();
+            sum_bubble += (double)(end - begin) / CLOCKS_PER_SEC;
+            std::cout << "Шаг : " << i << " | Кол-во прогонов : " << j
+                      << "       \r" << std::flush;
+        }
+        hoar << 1000000 * sum_hoar / repeats << " " << i << '\n';
+        bubble << 1000000 * sum_bubble / repeats << " " << i << '\n';
+    }
+
+    delete[] arr_bubble;
+    delete[] arr_hoar;
     return 0;
 }
