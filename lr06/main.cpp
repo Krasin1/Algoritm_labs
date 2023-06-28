5416c60a4ecea3a7a9a613624b86c9c3e5eb1649
Krasin1 <dunaevv84@gmail.com>
2023-06-28 18:04:20 +0000

    1 alg ready

--- lr06/main.cpp
+++ lr06/main.cpp
@@ -1,90 +1,179 @@
-#include <iostream>
-#include <vector>
 #include <ctime>
+#include <iostream>
 #include <random>
+#include <vector>
 
 using matr = std::vector<std::vector<bool>>;
 
 class Maze {
    public:
-    Maze() {
-        vert.reserve(4);
-        vec.reserve(4);
-        for (size_t i{}; i < 4; ++i) {
-            vert[i].reserve(4);
-            vec[i].reserve(4);
+    Maze() : counter(1), size(3) { resizeAllVec(size); }
+
+    Maze(size_t N) : counter(1), size(N) { resizeAllVec(size); }
+
+    Maze(size_t size, matr& a, matr& b)
+        : vert(a), horiz(b), counter(1), size(size) {}
+
+    void resizeAllVec(size_t N) {
+        vert.resize(N);
+        vec.resize(N);
+        horiz.resize(N);
+        for (size_t i{}; i < N; ++i) {
+            vert[i].resize(N);
+            vec[i].resize(N);
+            horiz[i].resize(N);
         }
     }
 
-    Maze(size_t N) {
-        vert.reserve(N);
-        vec.reserve(N);
+    void fillEmpty(size_t N) {
+        counter = 1;
+        size = N;
+        resizeAllVec(N);
         for (size_t i{}; i < N; ++i) {
-            vert[i].reserve(N);
-            vec[i].reserve(N);
-        }
-    }
-
-    Maze(matr& a, matr& b) : vert(a), horiz(b) {}
-
-    void fillEmptyRow(size_t N) {
-        counter = 0;
-
-        for (size_t i{}; i < N; ++i) {
-            vec[0][i] = 0;
+            for (size_t j{}; j < N; ++j) {
+                vec[i][j] = 0;
+                horiz[i][j] = false;
+                vert[i][j] = false;
+            }
         }
     }
 
     void assignUniqueSet(size_t N) {
-        for (size_t j{}; j < N; ++j) {
-            if (vec[0][j] == 0) {
-                vec[0][j] = (int)counter;
+        for (size_t j{}; j < size; ++j) {
+            if (vec[N][j] == 0) {
+                vec[N][j] = (int)counter;
+                counter++;
             }
-            counter++;
         }
     }
 
     void addingVerticalWalls(size_t row) {
-        for(size_t i {}; i < vert.size(); ++i){
+        for (size_t i{}; i < size - 1; ++i) {
             bool choise = rand() % 2;
+            if (choise || vec[row][i] == vec[row][i + 1]) {
+                vert[row][i] = true;
+            } else {
+                for (size_t j{}; j < size; ++j) {
+                    if (vec[row][j] == vec[row][i]) {
+                        vec[row][j] = vec[row][i + 1];
+                    }
+                }
+            }
         }
-
+        vert[row][size - 1] = true;
     }
 
-    void generate() {
-        size_t N = vert.size();
-        fillEmptyRow(N);
-        for (size_t i{}; i < N; ++i) {
-            assignUniqueSet(N);
-            addingVerticalWalls(i);
+    size_t checkSet(int Set, size_t row) {
+        size_t temp_cnt = 0;
+        for (size_t i{}; i < size; ++i) {
+            if (vec[row][i] == Set) {
+                temp_cnt++;
+            }
         }
+        return temp_cnt;
+    }
+
+    void addingHoirzWalls(size_t row) {
+        for (size_t i{}; i < size; ++i) {
+            bool choise = rand() % 2;
+            if (choise && checkSet(vec[row][i], row) != 1) {
+                horiz[row][i] = true;
+            }
+        }
+    }
+    size_t calculateHorizontalWalls(int element, size_t row) {
+        size_t countHorizontalWalls = 0;
+        for (size_t i = 0; i < size; i++) {
+            if (vec[row][i] == element && horiz[row][i] == false) {
+                countHorizontalWalls++;
+            }
+        }
+        return countHorizontalWalls;
+    }
+
+    void checkHorizWalls(size_t row) {
+        for(size_t i{}; i < size; ++i) {
+            if(calculateHorizontalWalls(vec[row][i], row) == 0) {
+                horiz[row][i] = false;
+            }
+        }
+    }
+
+    void prepareNewLine(size_t row) {
+        for (size_t i{}; i < size; ++i) {
+            if (horiz[row][i] == true) {
+                vec[row + 1][i] = 0;
+            } else {
+                vec[row + 1][i] = vec[row][i];
+            }
+        }
+    }
+
+    void checkEndLine() {
+        size_t endl = size - 1;
+        for (size_t i{}; i < size - 1; ++i) {
+            if (/*vert[endl][i] && */vec[endl][i]  != vec[endl][i + 1]) {
+                vert[endl][i] = false;
+                // merge sets
+                for (size_t j{}; j < size; ++j) {
+                    if (vec[endl][j] == vec[endl][i]) {
+                        vec[endl][j] = vec[endl][i + 1];
+                    }
+                }
+                // i++;
+            }
+            horiz[endl][i] = true;
+        }
+        horiz[endl][endl] = true;
+        std::cout << '\n';
+    }
+    void addEndLine() {
+        assignUniqueSet(size - 1);
+        addingVerticalWalls(size - 1);
+        printMaze();
+        checkEndLine();
+    }
+
+    void generate(size_t n) {
+        fillEmpty(n);
+        for (size_t i{}; i < size - 1; ++i) {
+            assignUniqueSet(i);
+            addingVerticalWalls(i);
+            addingHoirzWalls(i);
+            checkHorizWalls(i);
+            prepareNewLine(i);
+        }
+        addEndLine();
     }
 
     void printMaze() {
-        size_t N = vert.size();
-
         std::cout << "+";
-        for (size_t i{}; i < N; ++i) {
+        for (size_t i{}; i < size; ++i) {
             std::cout << "---+";
         }
         std::cout << "\n";
-        for (size_t i{}; i < N; ++i) {
+        for (size_t i{}; i < size; ++i) {
             std::cout << "|";
-            for (size_t j{}; j < N; ++j) {
-                if (vert[i][j] || j == N - 1)
+            for (size_t j{}; j < size; ++j) {
+                if (vert[i][j] || j == size - 1)
                     std::cout << "   |";
                 else
                     std::cout << "    ";
             }
             std::cout << "\n+";
-            for (size_t j{}; j < N; ++j) {
-                if (horiz[i][j] || i == N - 1)
+            for (size_t j{}; j < size; ++j) {
+                if (horiz[i][j] || i == size - 1)
                     std::cout << "---+";
                 else
                     std::cout << "   +";
             }
             std::cout << "\n";
         }
+        std::cout << " ";
+        for (size_t i{}; i < size; ++i) {
+            std::cout << vec[size - 1][i] << " ";
+        }
+        std::cout << '\n';
     }
 
    private:
@@ -92,6 +181,7 @@ class Maze {
     matr horiz;
     std::vector<std::vector<int>> vec;
     size_t counter;
+    size_t size;
 };
 
 auto main() -> int {
@@ -108,7 +198,10 @@ auto main() -> int {
         {1, 1, 0, 1},
         {1, 1, 1, 1},
     };
-    Maze maze(vert, horiz);
-    maze.printMaze();
+    Maze maze(4, vert, horiz);
+    // maze.printMaze();
+    Maze kek;
+    kek.generate(14);
+    kek.printMaze();
     return 0;
 }
