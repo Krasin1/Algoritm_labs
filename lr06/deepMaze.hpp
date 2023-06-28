4f9615613266fb8132e6bac829fa02941d1f62ba
Krasin1 <dunaevv84@gmail.com>
2023-06-28 20:15:18 +0000

    2 alg maze added

---
+++ lr06/deepMaze.hpp
@@ -0,0 +1,147 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <stack>
+
+#include <unistd.h>
+using matr = std::vector<std::vector<bool>>;
+
+class deepMaze {
+   public:
+    deepMaze(): size(3) { resizeAllVec(size); }
+
+    deepMaze(size_t N): size(N) { resizeAllVec(size); }
+
+    deepMaze(size_t size, matr& a, matr& b)
+        : vert(a), horiz(b), size(size) {}
+
+    void resizeAllVec(size_t N) {
+        vert.resize(N);
+        visited.resize(N);
+        horiz.resize(N);
+        for (size_t i{}; i < N; ++i) {
+            vert[i].resize(N);
+            visited[i].resize(N);
+            horiz[i].resize(N);
+        }
+    }
+
+    void fillEmpty(size_t N) {
+        size = N;
+        resizeAllVec(N);
+        for (size_t i{}; i < N; ++i) {
+            for (size_t j{}; j < N; ++j) {
+                visited[i][j] = false;
+                horiz[i][j] = true;
+                vert[i][j] = true;
+            }
+        }
+    }
+    bool checkCell(int x, int y) {
+        if(x < 0 || x > (int)size - 1 || y < 0 || y > (int)size - 1) {
+            return false;
+        }
+        return true;
+    }
+
+    std::pair<int, int> checkNeighbors(int x, int y) {
+        std::vector<std::pair<int, int>> neighbors;
+        bool top = checkCell(x, y - 1);
+        bool right = checkCell(x + 1, y);
+        bool bottom = checkCell(x, y + 1);
+        bool left = checkCell(x - 1, y);
+
+        if (top && !visited[y - 1][x]) {
+            neighbors.push_back(std::make_pair(x, y - 1));
+        }
+        if (right && !visited[y][x + 1]) {
+            neighbors.push_back(std::make_pair(x + 1, y));
+        }
+        if (bottom && !visited[y + 1][x]) {
+            neighbors.push_back(std::make_pair(x, y + 1));
+        } 
+        if (left && !visited[y][x - 1]) {
+            neighbors.push_back(std::make_pair(x - 1, y));
+        }
+        if (neighbors.size() != 0) {
+            size_t ran = (size_t)rand() % neighbors.size();
+            // std::cout<< ran << " " << neighbors[ran].first << " " << neighbors[ran].second << "\n";
+            return neighbors[ran];
+        } else {
+            return std::make_pair(-1, -1);
+        }
+    }
+
+    void removeWalls(int x1, int y1, int x2, int y2){
+        int dx = x2 - x1;
+        int dy = y2 - y1;
+        if (dx == 1) {
+            vert[(size_t)y1][(size_t)x1] = false;
+        } else if (dx == -1) {
+            vert[(size_t)y2][(size_t)x2] = false;
+        }
+        if (dy == 1) {
+            horiz[(size_t)y1][(size_t)x1] = false;
+        } else if (dy == -1) {
+            horiz[(size_t)y2][(size_t)x2] = false;
+        }
+    }
+
+    void generate(size_t N) {
+        std::pair<int, int> currnet_cell = std::make_pair(0, 0);
+        fillEmpty(N);
+        while(1) {
+            // usleep(500000);
+            visited[currnet_cell.second][currnet_cell.first] = true;
+            std::pair<int, int> next_cell = checkNeighbors(currnet_cell.first, currnet_cell.second);
+            // std::cout << currnet_cell.first << " " << currnet_cell.second << " | " << next_cell.first << " " << next_cell.second << " | " << stack.size() << '\n';
+            if (next_cell.first != -1 || next_cell.second != -1) {
+                visited[next_cell.second][next_cell.first] = true;
+                stack.push(currnet_cell);
+                removeWalls(currnet_cell.first, currnet_cell.second, next_cell.first, next_cell.second);
+                currnet_cell = next_cell;
+                printMaze();
+            } else if (!stack.empty()) {
+                    currnet_cell = stack.top();
+                    stack.pop();
+            } 
+            if (stack.empty()) {
+                break;
+            }
+        }
+    }
+
+
+    void printMaze() {
+        std::cout << "+";
+        for (size_t i{}; i < size; ++i) {
+            std::cout << "---+";
+        }
+        std::cout << "\n";
+        for (size_t i{}; i < size; ++i) {
+            std::cout << "|";
+            for (size_t j{}; j < size; ++j) {
+                if (vert[i][j] || j == size - 1)
+                    std::cout << "   |";
+                else
+                    std::cout << "    ";
+            }
+            std::cout << "\n+";
+            for (size_t j{}; j < size; ++j) {
+                if (horiz[i][j] || i == size - 1)
+                    std::cout << "---+";
+                else
+                    std::cout << "   +";
+            }
+            std::cout << "\n";
+        }
+        std::cout << '\n';
+    }
+
+   private:
+    matr vert;
+    matr horiz;
+    std::vector<std::vector<bool>> visited;
+    std::stack<std::pair<int, int>> stack;
+    size_t size;
+};
--- lr06/main.cpp
+++ lr06/main.cpp
@@ -1,188 +1,9 @@
 #include <ctime>
-#include <iostream>
 #include <random>
-#include <vector>
+#include "eulerMaze.hpp"
+#include "deepMaze.hpp"
 
 using matr = std::vector<std::vector<bool>>;
-
-class Maze {
-   public:
-    Maze() : counter(1), size(3) { resizeAllVec(size); }
-
-    Maze(size_t N) : counter(1), size(N) { resizeAllVec(size); }
-
-    Maze(size_t size, matr& a, matr& b)
-        : vert(a), horiz(b), counter(1), size(size) {}
-
-    void resizeAllVec(size_t N) {
-        vert.resize(N);
-        vec.resize(N);
-        horiz.resize(N);
-        for (size_t i{}; i < N; ++i) {
-            vert[i].resize(N);
-            vec[i].resize(N);
-            horiz[i].resize(N);
-        }
-    }
-
-    void fillEmpty(size_t N) {
-        counter = 1;
-        size = N;
-        resizeAllVec(N);
-        for (size_t i{}; i < N; ++i) {
-            for (size_t j{}; j < N; ++j) {
-                vec[i][j] = 0;
-                horiz[i][j] = false;
-                vert[i][j] = false;
-            }
-        }
-    }
-
-    void assignUniqueSet(size_t N) {
-        for (size_t j{}; j < size; ++j) {
-            if (vec[N][j] == 0) {
-                vec[N][j] = (int)counter;
-                counter++;
-            }
-        }
-    }
-
-    void addingVerticalWalls(size_t row) {
-        for (size_t i{}; i < size - 1; ++i) {
-            bool choise = rand() % 2;
-            if (choise || vec[row][i] == vec[row][i + 1]) {
-                vert[row][i] = true;
-            } else {
-                for (size_t j{}; j < size; ++j) {
-                    if (vec[row][j] == vec[row][i]) {
-                        vec[row][j] = vec[row][i + 1];
-                    }
-                }
-            }
-        }
-        vert[row][size - 1] = true;
-    }
-
-    size_t checkSet(int Set, size_t row) {
-        size_t temp_cnt = 0;
-        for (size_t i{}; i < size; ++i) {
-            if (vec[row][i] == Set) {
-                temp_cnt++;
-            }
-        }
-        return temp_cnt;
-    }
-
-    void addingHoirzWalls(size_t row) {
-        for (size_t i{}; i < size; ++i) {
-            bool choise = rand() % 2;
-            if (choise && checkSet(vec[row][i], row) != 1) {
-                horiz[row][i] = true;
-            }
-        }
-    }
-    size_t calculateHorizontalWalls(int element, size_t row) {
-        size_t countHorizontalWalls = 0;
-        for (size_t i = 0; i < size; i++) {
-            if (vec[row][i] == element && horiz[row][i] == false) {
-                countHorizontalWalls++;
-            }
-        }
-        return countHorizontalWalls;
-    }
-
-    void checkHorizWalls(size_t row) {
-        for(size_t i{}; i < size; ++i) {
-            if(calculateHorizontalWalls(vec[row][i], row) == 0) {
-                horiz[row][i] = false;
-            }
-        }
-    }
-
-    void prepareNewLine(size_t row) {
-        for (size_t i{}; i < size; ++i) {
-            if (horiz[row][i] == true) {
-                vec[row + 1][i] = 0;
-            } else {
-                vec[row + 1][i] = vec[row][i];
-            }
-        }
-    }
-
-    void checkEndLine() {
-        size_t endl = size - 1;
-        for (size_t i{}; i < size - 1; ++i) {
-            if (/*vert[endl][i] && */vec[endl][i]  != vec[endl][i + 1]) {
-                vert[endl][i] = false;
-                // merge sets
-                for (size_t j{}; j < size; ++j) {
-                    if (vec[endl][j] == vec[endl][i]) {
-                        vec[endl][j] = vec[endl][i + 1];
-                    }
-                }
-                // i++;
-            }
-            horiz[endl][i] = true;
-        }
-        horiz[endl][endl] = true;
-        std::cout << '\n';
-    }
-    void addEndLine() {
-        assignUniqueSet(size - 1);
-        addingVerticalWalls(size - 1);
-        printMaze();
-        checkEndLine();
-    }
-
-    void generate(size_t n) {
-        fillEmpty(n);
-        for (size_t i{}; i < size - 1; ++i) {
-            assignUniqueSet(i);
-            addingVerticalWalls(i);
-            addingHoirzWalls(i);
-            checkHorizWalls(i);
-            prepareNewLine(i);
-        }
-        addEndLine();
-    }
-
-    void printMaze() {
-        std::cout << "+";
-        for (size_t i{}; i < size; ++i) {
-            std::cout << "---+";
-        }
-        std::cout << "\n";
-        for (size_t i{}; i < size; ++i) {
-            std::cout << "|";
-            for (size_t j{}; j < size; ++j) {
-                if (vert[i][j] || j == size - 1)
-                    std::cout << "   |";
-                else
-                    std::cout << "    ";
-            }
-            std::cout << "\n+";
-            for (size_t j{}; j < size; ++j) {
-                if (horiz[i][j] || i == size - 1)
-                    std::cout << "---+";
-                else
-                    std::cout << "   +";
-            }
-            std::cout << "\n";
-        }
-        std::cout << " ";
-        for (size_t i{}; i < size; ++i) {
-            std::cout << vec[size - 1][i] << " ";
-        }
-        std::cout << '\n';
-    }
-
-   private:
-    matr vert;
-    matr horiz;
-    std::vector<std::vector<int>> vec;
-    size_t counter;
-    size_t size;
-};
 
 auto main() -> int {
     srand((unsigned)time(NULL));
@@ -201,7 +22,14 @@ auto main() -> int {
     Maze maze(4, vert, horiz);
     // maze.printMaze();
     Maze kek;
-    kek.generate(14);
-    kek.printMaze();
+    // kek.generate(14);
+    // kek.printMaze();
+
+    deepMaze lol;
+    lol.generate(10);
+    lol.printMaze();
+
+
+    
     return 0;
 }
